bf1b52f369278531b5576352394c8daef147e5f3
Jean-Michel Quint <jeanmichel.quint@gmail.com>
2017-09-26 16:47:28 +0000

    commit

--- modules/profiles/manifests/base.pp
+++ modules/profiles/manifests/base.pp
@@ -3,9 +3,11 @@ class profiles::base {
     servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
   }
 
-class { '::mysql::server':
-  root_password    => 'strongpassword',
-  override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+mysql::db { 'mydb':
+  user     => 'jmq',
+  password => 'jmq15',
+  host     => 'localhost',
+  grant    => ['SELECT', 'UPDATE'],
 }
 
 }
