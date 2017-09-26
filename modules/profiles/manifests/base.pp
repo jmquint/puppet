f0d552354936bf25083936ef0f102855af4d772e
Jean-Michel Quint <jeanmichel.quint@gmail.com>
2017-09-26 16:32:32 +0000

    commit

--- modules/profiles/manifests/base.pp
+++ modules/profiles/manifests/base.pp
@@ -2,4 +2,9 @@ class profiles::base {
   class { 'ntp':
     servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
   }
+
+  class { '::mysql::server':
+    root_password    => 'strongpassword',
+  }
+
 }
